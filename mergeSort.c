29ac840ccde6e5617113b1c5515f85952745fc36
chenganglist <1464125055@qq.com>
2016-07-06 17:19:44 +0800

    小段代码练习全部使用编辑器和gcc、g++在cygwin中完成,或者git自带的g++、gcc完成

---
+++ mergeSort.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int a[],int start,int mid,int end)
+{
+	int num = end-start+1;
+	int* tmp = (int*)malloc(num*sizeof(int));
+	
+
+
+	free(tmp);
+}
