d71bcb956d3f4045f5b59c8b37590c02e703db83
chenganglist <1464125055@qq.com>
2016-07-06 19:18:21 +0800

    16进制字符串转10进制

---
+++ hexStringToDecimal.java
@@ -0,0 +1,7 @@
+public class playGround {
+  public static void main(String[] args) {
+    String s = "0x405E";
+    int b = Integer.parseInt( s.replaceAll("^0[x|X]",""), 16);
+    System.out.println(b);
+  }
+}
