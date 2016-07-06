5f341717bbc2f445af4c2647cdd7321977f2ae32
chenganglist <1464125055@qq.com>
2016-07-06 10:08:05 +0800

    从0到num，不设置哨兵的插入排序

---
+++ insertSort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertSort(int a[],int num)
+{
+	int i,j;
+	for(i=0;i<num-1;i++)
+	{
+		for(j=i+1;j>0;j--)
+		{
+			if(a[j] < a[j-1])
+			{
+				int tmp = a[j];
+				a[j] = a[j-1];
+				a[j-1] = tmp;
+			}else
+			{
+				break;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int num;
+	scanf("%d",&num);
+	int i;
+	int *a = (int*)malloc( num*sizeof(int) );
+	for(i=0;i<num;i++)
+	{
+		scanf("%d",a+i);
+	}
+
+	insertSort(a,num);
+
+	for(i=0;i<num;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	return 0;
+}
