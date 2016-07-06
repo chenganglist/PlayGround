4db33199acf59461391dc5f1c907d0d95a92ee31
chenganglist <1464125055@qq.com>
2016-07-06 15:39:24 +0800

    快速排序初版

--- quickSort.c
+++ quickSort.c
@@ -6,26 +6,34 @@ int findMiddle(int a[],int start,int end)
 {
 	int i=start,j=end,mid=start;
 
-	while(i<j)
+	while(start<end)
 	{
 		for(j=end;j>start;j--)
 		{
 			if(a[j]<a[mid])
 			{
-				end = j;
 				int tmp = a[j];
 				a[j] = a[mid];
 				a[mid] = tmp;
+				mid = j;
+				end = j;
 			}
 		}
 
 
 		for(i=start;i<end;i++)
 		{
-
+			if(a[i]>a[mid])
+			{
+				int tmp = a[i];
+				a[i] = a[mid];
+				a[mid] = tmp;
+				mid = i;
+				start = i;
+			}
 		}
 	}
-	mid = i;
+
 	return mid;
 }
 
@@ -38,4 +46,27 @@ void quickSort(int a[],int start,int end)
 		quickSort(a,start,mid);
 		quickSort(a,mid+1,end)
 	}
+}
+
+int main()
+{
+	int num;
+	scanf("%d",&num);
+	int i;
+	int *a = (int*)malloc(num*sizeof(a));
+
+	for(i=0;i<num;i++)
+	{
+		scanf("%d",a+i);
+	}
+
+	quickSort(a,0,n-1);
+
+	for(i=0;i<num;i++)
+	{
+		printf("%d ",a[i]);
+	}
+
+	free(a);
+	return 0;
 }
