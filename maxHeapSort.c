374dc90be517a9e3fa3818178ffbad35aacc6182
chenganglist <1464125055@qq.com>
2016-07-06 21:25:25 +0800

    添加最大堆排序实现框架

---
+++ maxHeapSort.c
@@ -0,0 +1,20 @@
+//从第i个节点向下调整节点
+void ajustMaxHeap(int a[],int i,int n)
+{
+
+}
+
+
+//建立最大堆
+void buildMaxHeap(int a[],int n)
+{
+
+}
+
+
+//每次选择最大元素，完成最大堆排序
+void maxHeapSort(int a[],int n)
+{
+
+	
+}
