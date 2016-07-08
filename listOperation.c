063384eb5776279a6f547e675d04dc5b5ae23e7d
chenganglist <1464125055@qq.com>
2016-07-08 16:03:09 +0800

    增加链表操作实现说明文件

--- listOperation.c
+++ listOperation.c
@@ -37,21 +37,47 @@ node* createList()
 	return head;
 }
 
+
+
 void printList(node* head)
 {
 	puts("打印的链表如下所示：");
-	while(head!=NULL)
+	node* cur = head;
+	while(cur!=NULL)
 	{
-		printf("%d ",head->data);
-		head =  head->next;
+		printf("%d ",cur->data);
+		cur =  cur->next;
+	}
+	puts("");
+}
+
+
+
+
+/*整个程序结束后，或者专业的说，进程结束后，申请的内存是会被释放的，
+但是这要依靠底层操作系统内存管理模块的实现机制，不同的实现机制释放
+内存的时间也不相同。不过，作为一个良好的习惯，
+申请内存和释放内存应该成对出现。*/
+
+void deleteList(node* head)
+{
+	puts("删除链表，防止内存泄漏");
+	node* cur = head;
+	while(cur!=NULL)
+	{
+		node* tmp = cur->next;
+		free(cur);
+		cur = tmp;
 	}
 }
+
 
 
 int main()
 {
 	node* listHead = createList();
 	printList(listHead);
+	deleteList(listHead);
 	return 0;
 }
 
