7d24de87265a98d17a61894c838667e7364cb4e7
Sublime2Coding <1464125055@qq.com>
2016-07-08 22:16:44 +0800

    添加插入功能：在指定位置之后插入新元素

--- listOperation.c
+++ listOperation.c
@@ -52,13 +52,40 @@ void printList(node* head)
 }
 
 
+//在指定位置插入新元素
+void insertList(node* head,int insertPos,node* newElement)
+{
+	if(insertPos<0)
+	{
+		puts("unsupposrted position");
+		return;
+	}
+	int pos = 0;
+	node* cur = head;
+	while(cur!=NULL)
+	{
+		if(insertPos == pos+1 )
+		{
+			newElement->next = cur->next;
+			cur->next = newElement;
+			return;
+		}
+		cur = cur->next;
+		pos++;
+	}
+	if(pos<insertPos)
+	{
+		puts("unsupposrted position");
+		return;
+	}
+}
 
 
-/*整个程序结束后，或者专业的说，进程结束后，申请的内存是会被释放的，
-但是这要依靠底层操作系统内存管理模块的实现机制，不同的实现机制释放
+/*整个程序结束后，或者专业的说，进程结束后，
+申请的内存是会被释放的，但是这要依靠底层操作系统
+内存管理模块的实现机制，不同的实现机制释放
 内存的时间也不相同。不过，作为一个良好的习惯，
 申请内存和释放内存应该成对出现。*/
-
 void deleteList(node* head)
 {
 	puts("删除链表，防止内存泄漏");
@@ -76,6 +103,16 @@ void deleteList(node* head)
 int main()
 {
 	node* listHead = createList();
+	puts("创建的链表为：");
+	printList(listHead);
+
+	node* newElement = (node*)malloc(sizeof(node));
+	newElement->data = 100;
+	newElement->next = NULL;
+	int insertPos =  5;
+	insertList(listHead,insertPos,newElement);
+
+	puts("插入新元素后链表为：");
 	printList(listHead);
 	deleteList(listHead);
 	return 0;
